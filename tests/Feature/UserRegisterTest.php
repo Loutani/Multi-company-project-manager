63d641121e62a2c1ae14474edf94db3898d99f07
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2023-03-08 12:24:43 +0100

    integrate the register new user test

--- tests/Feature/UserRegisterTest.php
+++ tests/Feature/UserRegisterTest.php
@@ -2,19 +2,39 @@
 
 namespace Tests\Feature;
 
+use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class UserRegisterTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
-     * A basic feature test example.
+     * Guest can get register view
      */
     public function test_register_form_can_be_rendered(): void
     {
-        $view = $this->view('auth.register');
+        $view = $this->withViewErrors([])->view('auth.register');
  
         $view->assertSee('Sign Up');
+    }
+
+    /**
+     * Guest can register and redirect to view
+     */
+    public function test_register_action_must_return_success_message()
+    {
+        $registerData = [
+            'name' => 'Mohammed',
+            'email' => 'mohammed@company-dz.com',
+            'password' => 'mohammed1234',
+            'password_confirmation' => 'mohammed1234',
+        ];
+
+        $response = $this->call('POST', 'register', $registerData);
+
+        $response->assertStatus(302);
     }
 }
