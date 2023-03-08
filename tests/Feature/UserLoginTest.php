61689d01d0edd0c3dd742cbf373fea5d150844c2
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2023-03-08 13:55:22 +0100

    integrate the login case test

--- tests/Feature/UserLoginTest.php
+++ tests/Feature/UserLoginTest.php
@@ -3,12 +3,14 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class UserLoginTest extends TestCase
 {
     use RefreshDatabase;
+    use DatabaseMigrations;
 
     /**
      * user should see login view page
@@ -18,5 +20,20 @@ class UserLoginTest extends TestCase
         $response = $this->withViewErrors([])->view('auth.login');
 
         $response->assertSee('Sign In');
+    }
+
+    /**
+     * if the user enter correct email and password should login to its account
+     */
+    public function test_user_can_login_to_its_account()
+    {
+        $registerData = [
+            'email' => 'mohammed@company-dz.com',
+            'password' => 'mohammed1234'
+        ];
+
+        $response = $this->call('POST', 'login', $registerData);
+
+        $response->assertStatus(302);
     }
 }
