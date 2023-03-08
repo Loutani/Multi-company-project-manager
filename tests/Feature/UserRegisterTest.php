1752fe3afd24d443692b759df9aff19e37efb8b4
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2023-03-08 12:36:18 +0100

    complete the register action test cases

--- tests/Feature/UserRegisterTest.php
+++ tests/Feature/UserRegisterTest.php
@@ -37,4 +37,96 @@ class UserRegisterTest extends TestCase
 
         $response->assertStatus(302);
     }
+
+    /**
+     * if we don't send email we should catch it as error
+     */
+    public function test_email_is_required_should_show_if_you_dont_send_it()
+    {
+        $registerData = [
+            'name' => 'Mohammed',
+            'password' => 'mohammed1234',
+            'password_confirmation' => 'mohammed1234',
+        ];
+
+        $response = $this->call('POST', 'register', $registerData);
+
+        $response->assertStatus(302);
+
+        $response->assertSessionHasErrors(['email' => 'The email field is required.']);
+    }
+
+    /**
+     * if we don't send name we should catch it as error
+     */
+    public function test_name_is_required_should_show_if_you_dont_send_it()
+    {
+        $registerData = [
+            'email' => 'mohammed@company-dz.com',
+            'password' => 'mohammed1234',
+            'password_confirmation' => 'mohammed1234',
+        ];
+
+        $response = $this->call('POST', 'register', $registerData);
+
+        $response->assertStatus(302);
+
+        $response->assertSessionHasErrors(['name' => 'The name field is required.']);
+    }
+
+    /**
+     * if we don't send password we should catch it as error
+     */
+    public function test_password_is_required_should_show_if_you_dont_send_it()
+    {
+        $registerData = [
+            'name' => 'Mohammed',
+            'email' => 'mohammed@company-dz.com',
+            'password_confirmation' => 'mohammed1234',
+        ];
+
+        $response = $this->call('POST', 'register', $registerData);
+
+        $response->assertStatus(302);
+
+        $response->assertSessionHasErrors(['password' => 'The password field is required.']);
+    }
+
+    /**
+     * if the password and password_confirmation not match we should catch it as error
+     */
+    public function test_password_is_miss_match_should_show()
+    {
+        $registerData = [
+            'name' => 'Mohammed',
+            'email' => 'mohammed@company-dz.com',
+            'password' => 'mohammed1234',
+            'password_confirmation' => 'mohammed1234dsqdsqsq',
+        ];
+
+        $response = $this->call('POST', 'register', $registerData);
+
+        $response->assertStatus(302);
+
+        $response->assertSessionHasErrors(['password' => 'The password field confirmation does not match.']);
+    }
+
+    /**
+     * if the format of email is not valid we should catch it as error
+     */
+    public function test_email_valid_format_should_show()
+    {
+        $registerData = [
+            'name' => 'Mohammed',
+            'email' => 'mohammed-company-dz.com',
+            'password' => 'mohammed1234',
+            'password_confirmation' => 'mohammed1234dsqdsqsq',
+        ];
+
+        $response = $this->call('POST', 'register', $registerData);
+
+        $response->assertStatus(302);
+
+        $response->assertSessionHasErrors(['email' => 'The email field must be a valid email address.']);
+    }
 }
