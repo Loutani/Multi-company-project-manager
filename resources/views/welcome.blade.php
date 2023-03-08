ab81875f2693fa3f4f695f1c65e82aefdfb9e1e9
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2023-03-08 13:54:16 +0100

    integrate landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,1 @@
-@extends('layouts.index')
-
-@section('title', 'Dashboard')
-
-@section('content')
-    <div class="content-page">
-        <div class="container-fluid">
-
-        </div>
-    </div>
-@endsection
+<h1>Just Landing Page</h1>
