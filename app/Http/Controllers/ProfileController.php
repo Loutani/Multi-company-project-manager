680fb811971e43bb11662f8625c1f1d027e495af
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2023-03-09 08:30:12 +0100

    integrate the edit action

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Profile;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -17,5 +18,13 @@ class ProfileController extends Controller
         $user = User::find(Auth::user()->id)->with('profile')->get()->first();
 
         return View('profile.index', compact('user'));
+    }
+
+    public function edit(Profile $id)
+    {
+        $userId = Auth()->user()->id;
+        $profile = $id;
+
+        return View('profile.edit', compact('profile', 'userId'));
     }
 }
